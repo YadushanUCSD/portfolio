85025111ab3fde2cdcdbf4027e6feec150c2f00a
YadushanUCSD <ythillainathan@ucsd.edu>
2026-04-17 15:42:30 -0700

    updated navigation with js

---
+++ global.js
@@ -0,0 +1,75 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// const navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+
+//   currentLink.classList?.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/YadushanUCSD', title: 'GitHub' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/website/";
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
+    );
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    nav.append(a);
+  }
+
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+                <option value="light dark">Automatic</option>
+			    <option value="light">Light</option>
+			    <option value="dark">Dark</option>
+          </select>
+      </label>`,
+  );
+
+  const select = document.querySelector('.color-scheme select');
+  function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', colorScheme);
+    select.value = colorScheme;
+  }
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    setColorScheme(event.target.value);
+    localStorage.colorScheme = event.target.value;
+  });
+  if ("colorScheme" in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+  }
