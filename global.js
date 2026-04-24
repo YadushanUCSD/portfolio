224e30813408cf2e4a55318da56ff14e3d8c1e53
YadushanUCSD <ythillainathan@ucsd.edu>
2026-04-24 13:36:35 -0700

    completed lab 4

--- global.js
+++ global.js
@@ -73,3 +73,36 @@ for (let p of pages) {
   if ("colorScheme" in localStorage) {
     setColorScheme(localStorage.colorScheme);
   }
+
+  export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        console.log(response)
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
+  export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+    projects.forEach(project => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <h3>${project.title}</h3>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>`;
+        containerElement.appendChild(article);
+    });
+  }
+
+  export async function fetchGithubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+  }
+
+  
